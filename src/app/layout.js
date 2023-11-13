e3effdb23127ab0383401127338f201193d5183c
dhommen <dh@orbiter.de>
2023-11-13 19:18:01 +0100

    feat: add frooter and imprint

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Footer from "@/app/components/footer"
 
 export const metadata = {
   title: 'Daniel Hommen',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
